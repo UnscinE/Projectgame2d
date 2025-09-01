c837a81f511584f3229ff0f2cdcae092ad1cd460
Tanawat Tanatka <tanawat.tan@kkumail.com>
2025-09-02 02:29:01 +0700

    Add gameover map3 almost finish

---
+++ Gameres/Script/map_3.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var player = $Player
+@onready var spawn_point = $SpawnPoint
+
+func _ready():
+	player.global_position = spawn_point.global_position
+
+func respawn_player():
+	player.global_position = spawn_point.global_position
+
+func update_spawn(new_spawn: Marker2D):
+	spawn_point = new_spawn
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
